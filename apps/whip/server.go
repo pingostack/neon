4e362aaeab2c5937eff515b7611b8201bf71a6d4
im-pingo <cczjp89@gmail.com>
2024-07-30 18:38:39 +0800

    test options

--- apps/whip/server.go
+++ apps/whip/server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"strings"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/let-light/gomodule"
 	feature_rtc "github.com/pingostack/neon/features/rtc"
@@ -36,17 +37,38 @@ func NewSignalServer(ctx context.Context, httpParams httpserv.HttpParams, logger
 }
 
 func (ss *SignalServer) Start() error {
+	// 配置 CORS 中间件
+	corsConfig := cors.Config{
+		AllowOrigins:     []string{"*"}, // 允许所有来源
+		AllowMethods:     []string{"OPTIONS", "GET", "POST", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization", "If-Match"},
+		ExposeHeaders:    []string{"Link"},
+		AllowCredentials: true,
+	}
+
+	ss.ss.DefaultRouter().Use(cors.New(corsConfig))
+
 	ss.ss.DefaultRouter().Use(func(gc *gin.Context) {
 		gc.Writer.Header().Set("Access-Control-Allow-Origin", strings.Join(ss.httpParams.AllowOrigin, " ,"))
 		gc.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	})
 
-	ss.ss.DefaultRouter().POST("/:app/:stream/whip", ss.handlePostWhip)
-	ss.ss.DefaultRouter().OPTIONS("/:app/:stream/whip", ss.handleOptions)
-	ss.ss.DefaultRouter().POST("/:app/:stream/whep", ss.handlePostWhep)
-	ss.ss.DefaultRouter().OPTIONS("/:app/:stream/whep", ss.handleOptions)
+	ss.ss.DefaultRouter().POST("/whip/:app/:stream/", ss.handlePostWhip)
+	ss.ss.DefaultRouter().OPTIONS("/whip/:app/:stream/:method", ss.handleOptions)
+	ss.ss.DefaultRouter().PATCH("/whip/:app/:stream/:method/:secret", ss.handlePatchWhip)
+	ss.ss.DefaultRouter().DELETE("/whip/:app/:stream/:method/:secret", ss.handleDeleteWhip)
 
-	return ss.ss.Start(ss.handleRequest)
+	ss.ss.DefaultRouter().POST("/whep/:app/:stream/", ss.handlePostWhep)
+	ss.ss.DefaultRouter().OPTIONS("/whep/:app/:stream/:method", ss.handleOptions)
+	ss.ss.DefaultRouter().PATCH("/whep/:app/:stream/:method/:secret", ss.handlePatchWhep)
+	ss.ss.DefaultRouter().DELETE("/whep/:app/:stream/:method/:secret", ss.handleDeleteWhep)
+
+	ss.ss.DefaultRouter().OPTIONS("", func(ctx *gin.Context) {
+		ctx.Writer.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PATCH, DELETE")
+		ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, If-Match")
+		ctx.Writer.WriteHeader(http.StatusNoContent)
+	})
+	return ss.ss.Start()
 }
 
 func (ss *SignalServer) Close() error {
@@ -69,12 +91,14 @@ func (ss *SignalServer) handlePostWhep(gc *gin.Context) {
 
 }
 
-func (ss *SignalServer) handleRequest(gc *gin.Context) {
-	if gc.Request.Method == http.MethodOptions &&
-		gc.Request.Header.Get("Access-Control-Request-Method") != "" {
-		gc.Writer.Header().Set("Access-Control-Allow-Methods", strings.Join(ss.ss.AllowMethods(), ", "))
-		gc.Writer.Header().Set("Access-Control-Allow-Headers", strings.Join(ss.ss.AllowHeaders(), ", "))
-		gc.Writer.WriteHeader(http.StatusNoContent)
-		return
-	}
+func (ss *SignalServer) handlePatchWhep(gc *gin.Context) {
+}
+
+func (ss *SignalServer) handleDeleteWhep(gc *gin.Context) {
+}
+
+func (ss *SignalServer) handlePatchWhip(gc *gin.Context) {
+}
+
+func (ss *SignalServer) handleDeleteWhip(gc *gin.Context) {
 }
